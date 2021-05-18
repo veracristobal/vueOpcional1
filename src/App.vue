0c4cbdf0eb355caccefb2ffa812938d06a3f025d
unknown <crizverar@gmail.com>
2021-05-18 09:02:54 -0400

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="http://blog.desafiolatam.com/wp-content/uploads/2015/03/desafio-latam-logonegro.png">
+  <DataTable/>
+</template>
+
+<script>
+import DataTable from './components/DataTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DataTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <h1>{{ mensaje }}</h1>
+    <div id="app" class="container-fluid d-flex justify-content-center align-items-center">
+
+        <table id="customers" class="w-50">
+            <tr>
+                <th v-for='head in dataHeaders' :key="head">{{head}}</th>
+            </tr>
+
+            <tr v-for='user in usuarios' :key="user">
+                <td>{{user.id}}</td>
+                <td> {{user.nombre}} </td>
+                <td>{{user.apellido}}</td>
+                <td> {{user.run}}</td>
+                <td> {{user.nacimiento}}</td>
+                <td> {{user.edad}}</td>
+
+            </tr>
+        </table>
+    
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  data(){
+      return{
+          
+        dataHeaders: ['#', 'Nombre', 'Apellido', 'RUN', 'Nacimiento', 'Edad'],
+        usuarios: [{
+                id: 1,
+                nombre: 'Roberto',
+                apellido: 'Gomez',
+                run: '12345678-9',
+                nacimiento: '12/05/87',
+                edad: 34
+            },
+            {
+                id: 2,
+                nombre: 'Daniel',
+                apellido: 'Rivas',
+                run: '1238445678-1',
+                nacimiento: '02/08/94',
+                edad: 27
+            },
+            {
+                id: 3,
+                nombre: 'Victor',
+                apellido: 'Hugo',
+                run: '1234534-k',
+                nacimiento: '12/05/77',
+                edad: 55
+            }
+        ]
+    }
+      
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
